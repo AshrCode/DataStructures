0ae83db04aa55d7f64fa767b8f06061c37055161
Ashton <khizerashrafi@live.com>
2020-02-02 13:59:54 +0500

    Add project files.

---
+++ TreeVariants/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace TreeVariants
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+        public Person()
+        { }
+        public Person(int id, string name, string role)
+        {
+            Id = id;
+            Name = name;
+            Role = role;
+        }
+    }
+
+}
---
+++ TreeVariants/TreeNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeVariants
+{
+    public class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public TreeNode<T> Parent { get; set; }
+        public List<TreeNode<T>> Children {get; set;}
+
+        /// <summary>
+        /// Calculates the distance to the root noe
+        /// </summary>
+        /// <returns>The height of the current node.</returns>
+        public int GetHeight()
+        {
+            int height = 1;
+            TreeNode<T> current = this;
+            while(current.Parent != null)
+            {
+                height++;
+                current.Parent = Parent;
+            }
+
+            return height;
+        }
+    }
+}
