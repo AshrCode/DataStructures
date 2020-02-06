2caed779ae2f42df0f68fbcabaec94fe9939a6c7
Ashton <khizerashrafi@live.com>
2020-02-06 11:56:21 +0500

    Added project for Linked List data structure.

---
+++ LinkedListVariants/Node.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListVariants
+{
+    public class Node
+    {
+        public Node NextNode { get; set; }
+
+        public int Data { get; set; }
+
+        public Node(int Data)
+        {
+            this.Data = Data;
+            this.NextNode = null;
+        }
+
+        public void Print()
+        {
+            Console.Write("|" + this.Data + "|-> ");
+
+            if (this.NextNode != null)
+            {
+                NextNode.Print();
+            }
+        }
+
+        public void AddToEnd(int Data)
+        {
+            if(NextNode == null)
+            {
+                NextNode = new Node(Data);
+            }
+            else
+            {
+                NextNode.AddToEnd(Data);
+            }
+        }
+    }
+}
---
+++ LinkedListVariants/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LinkedListVariants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is how Linked List data structure works.");
+
+            // One of adding multiple nodes
+            //Node headNode = new Node(12);
+            //headNode.NextNode = new Node(11);
+            //headNode.NextNode.NextNode = new Node(13);
+            //headNode.NextNode.NextNode.NextNode = new Node(7);
+
+            // Recursive way of adding node to the end of the chain
+            Node headNode = new Node(6);
+            headNode.AddToEnd(17);
+            headNode.AddToEnd(27);
+            headNode.AddToEnd(13);
+
+            headNode.Print();
+
+
+            // To hold the output
+            Console.Read();
+        }
+    }
+}
